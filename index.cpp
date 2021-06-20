c95580809c3dfb567938d688379c02be26425936
kedigucuk01 <65870901+kedigucuk01@users.noreply.github.com>
2021-06-20 20:04:28 +0300

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void endeks(double kilo, double boy){
+
+    try{
+        if(kilo <= 0 || boy <= 0) throw 1;
+
+        else{
+
+            double result = kilo / pow(( boy/ 100), 2);
+
+
+            if (result <= 20)
+                cout << "Ideal kilonuzun altindasiniz." << endl;
+
+            else if(result > 20 && result <= 25)
+                cout << "Ideal kilodasiniz." << endl;
+
+            else if(result > 25 && result <= 30)
+                cout << "Ideal kilonun ustundesiniz." << endl;
+
+            else if(result > 30)
+                cout << "Obezsiniz." << endl;
+            
+
+            cout << "Vucut kitle endeksiniz: " << result << endl;
+
+        }
+    }
+
+    catch(int num){
+        cout << "Lutfen gecerli boy ve kilo verisi giriniz." << endl;
+    }
+
+}
+
+int main(){
+
+    while(true){
+
+        string sk, sb;
+        double k, b;
+
+        cout << "\n" << "Lutfen kilonuzu giriniz(kg): ";
+        getline(cin, sk);
+
+        cout << "Lutfen boyunuzu giriniz(cm): ";
+        getline(cin, sb);
+
+        try {
+            k = stod(sk);
+            b = stod(sb);
+        
+        } catch(invalid_argument& e){
+            cout << "Lutfen sayi giriniz." << endl;
+            continue;
+            
+    }
+
+        endeks(k,b);
+    }
+    
+    
+    return 0;
+}
